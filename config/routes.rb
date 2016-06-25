2db1b191ba73138ad09bfbefa919499a69903bed
Larry Reid <lcreid@jadesystems.ca>
2016-06-25 11:39:51 -0700

    Saving work before updating Rails.

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resources :cf0925s
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
