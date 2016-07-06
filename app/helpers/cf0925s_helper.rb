16f40fa817fd8743676eafa169ba9a0b1dde369a
Larry Reid <lcreid@jadesystems.ca>
2016-07-05 20:59:51 -0600

    Partial functionality.

--- app/helpers/cf0925s_helper.rb
+++ app/helpers/cf0925s_helper.rb
@@ -1,7 +1,19 @@
 module Cf0925sHelper
-  def show_field(field)
-    "<p id=\"#{field}\">#{field.to_s.titlecase}: " \
-      "<span class=\"value\">#{@cf0925.send(field)}</span></p>"
+  def show_field(field, width = 4)
+    "<div id=\"#{field}\" class=\".col-md-#{width}\">" \
+      "#{field.to_s.titlecase}: " \
+      "<span class=\"value\">#{@cf0925.send(field)}</span>" \
+    '</div>'
       .html_safe
+  end
+
+  def form_row
+    '<div class="row">'.html_safe
+    yield
+    '</div>'.html_safe
+  end
+
+  def form_field(field, width = 4)
+    show_field(field, width)
   end
 end
