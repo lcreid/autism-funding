01ce2dec7a489ffcd026fa3a21e8f1fc0a04c1ea
Larry Reid <lcreid@jadesystems.ca>
2016-07-13 17:32:51 -0700

    Make home page default and welcome page if not logged in.
    Added an application before filter to check signed in status.
    Had to do a trick to allow Devise to still sign in or up.
    Cleaned up a few places where there were controller-level checks
    for signed in.

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Rails.application.routes.draw do
   #---- Website Root -----------------------------------------------------------
-  root 'welcome#index'
+  root 'home#index'
+  get 'home/index'
 
   get 'welcome/index'
 
   get 'other_resources/index'
   get 'my_profile/index'
 
-  get 'home/index'
-
   devise_for :users
 
   resources :cf0925s
-
   resources :funded_people
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
