732214d64f009eecad42649745690cf4992ff9d3
Larry Reid <lcreid@jadesystems.ca>
2016-08-03 13:22:06 -0700

    Prototype E.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :cf0925s
-  resources :funded_people
+  resources :funded_people do
+    get :all_forms
+    get :all_invoices
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
