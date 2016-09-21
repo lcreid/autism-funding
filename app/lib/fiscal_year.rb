f1adc79e91879b591c7fa220e78537f89b1c5254
Larry Reid <lcreid@jadesystems.ca>
2016-09-21 12:31:00 -0700

    Refactor fiscal_year to module.

---
+++ app/lib/fiscal_year.rb
@@ -0,0 +1,12 @@
+##
+# Fiscal year for models that belong to a FundedPerson.
+# The model has to respond to start_date and funded_person.
+module FiscalYear
+  ##
+  # Return the fiscal year of the model, defined as the fiscal year of the
+  # start date for service.
+  def fiscal_year
+    return nil unless start_date
+    funded_person.fiscal_year(start_date)
+  end
+end
