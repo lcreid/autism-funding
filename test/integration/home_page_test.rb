de7e8bf66c2c4710da4eb2212398e1b6f6c31d44
Larry Reid <lcreid@jadesystems.ca>
2016-09-21 22:48:55 -0700

    Fill in year menu on home page.

--- test/integration/home_page_test.rb
+++ test/integration/home_page_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
 require 'capybara/poltergeist'
+
 class HomePageTest < CapybaraTest
   include TestSessionHelpers
 
@@ -32,5 +33,19 @@ class HomePageTest < CapybaraTest
     # assert_content 'Funding spent', count: 1
     click_link(last_child.my_name)
     assert_no_selector("#collapse-#{last_child.id}[style=\"height: 0px;\"]")
+  end
+
+  test 'fiscal year menu' do
+    fill_in_login(users(:years))
+    assert_current_path root_path
+
+    within "#collapse-#{funded_people(:one_fiscal_year).id}" do
+      select '2017', from: 'Year'
+    end
+
+    within "#collapse-#{funded_people(:two_fiscal_years).id}" do
+      select '2015-2016', from: 'Year'
+      select '2016-2017', from: 'Year'
+    end
   end
 end
