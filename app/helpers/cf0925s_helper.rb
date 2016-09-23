3b3bd569784a0c4cba8403a72777508ec785dda8
Larry Reid <lcreid@jadesystems.ca>
2016-09-22 19:19:11 -0700

    Use more Bootstrap formatting in forms.

--- app/helpers/cf0925s_helper.rb
+++ app/helpers/cf0925s_helper.rb
@@ -1,3 +1,5 @@
+##
+# Field helpers for BC request to pay forms.
 module Cf0925sHelper
   def show_field(field, width = 4, opts = {}, &block)
     wrap_field(width) do
@@ -13,7 +15,7 @@ module Cf0925sHelper
 
   def form_row(&block)
     raise ArgumentError, 'Missing block' unless block_given?
-    content_tag :div, class: 'row' do
+    content_tag :div, class: 'form-inline' do
       capture(&block)
     end
   end
