978b80f3c332a5d205b3d70bf96ccc8678f2720b
Larry Reid <lcreid@jadesystems.ca>
2016-09-22 20:07:33 -0700

    Add FormBuilder method for errors.

---
+++ app/helpers/form_builder.rb
@@ -0,0 +1,33 @@
+module ActionView
+  module Helpers
+    ##
+    # Add a method to FormBuilder to set up an error field for an input field.
+    class FormBuilder
+      ##
+      # Create a place to put error messages for a field.
+      def error_message_for(field_name)
+        if object.errors[field_name].present?
+          model_name = object.class.name.downcase
+          id_of_element           = "error_#{model_name}_#{field_name}"
+          target_elem_id          = "#{model_name}_#{field_name}"
+          class_name              = 'signup-error alert alert-danger'
+          # error_declaration_class = 'has-signup-error'
+
+          content_tag :div,
+                      id: id_of_element,
+                      for: target_elem_id,
+                      class: class_name do
+            object.errors[field_name].join(', ')
+          end
+          # '<!-- Later JavaScript to add class to the parent element -->'\
+          # '<script>'\
+          #     'document.onreadystatechange = function(){'\
+          #       "$('##{id_of_element}').parent()"\
+          #       ".addClass('#{error_declaration_class}');"\
+          #     '}'\
+          # '</script>'.html_safe
+        end
+      end
+    end
+  end
+end
