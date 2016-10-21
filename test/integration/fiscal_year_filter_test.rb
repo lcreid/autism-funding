52cf7897aa5e0e054c7f0f3ea484b67eadf29f2a
Larry Reid <lcreid@jadesystems.ca>
2016-10-21 15:16:49 -0700

    Commit so I can merge.

--- test/integration/fiscal_year_filter_test.rb
+++ test/integration/fiscal_year_filter_test.rb
@@ -35,4 +35,35 @@ class FiscalYearFilterTest < PoltergeistTest
       end
     end
   end
+
+  test 'Set fiscal year based on last RTP' do
+    fill_in_login(user = users(:years))
+
+    child = user.funded_people.select { |x| x.name_first == 'Two' }.first
+    year_selector = "year_#{child.id}"
+    within "#collapse-#{child.id}" do
+      assert_select(year_selector, selected: '2016-2017')
+      click_link('New Request to Pay')
+    end
+
+    # sleep 5
+    # assert_current_path new_funded_person_cf0925_path(child)
+    # puts find('#cf0925_service_provider_service_start').inspect
+    # assert_selector '#cf0925_service_provider_service_start'
+    # assert_selector '#cf0925_service_provider_service_end'
+    # assert_content 'Parent/Guardian Information'
+    # fill_in 'Start Date', with: '2015-09-01'
+    # fill_in 'End Date', with: '2015-12-31'
+    find('#cf0925_service_provider_service_start').set('2015-09-01')
+    find('#cf0925_service_provider_service_start').set('2015-12-31')
+    click_button 'Save'
+    click_link 'Home'
+    assert_current_path home_index_path
+    # puts page.html
+
+    within "#collapse-#{child.id}" do
+      # assert_select(year_selector, selected: '2016-2017')
+      assert_select(year_selector, selected: '2015-2016')
+    end
+  end
 end
