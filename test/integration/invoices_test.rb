b5cadd44d3a13a64c88afc0832fe0a1e475729f4
Larry Reid <lcreid@jadesystems.ca>
2016-11-27 13:52:19 -0800

    Wrestling with Capybara.

--- test/integration/invoices_test.rb
+++ test/integration/invoices_test.rb
@@ -20,7 +20,9 @@ class InvoicesTest < PoltergeistTest
     fill_in 'Service End', with: '2017-01-31'
 
     # puts "INSIDE: #{find('#invoice_cf0925').native}"
-    # puts "INSIDE: #{page.evaluate_script("document.getElementById('invoice_cf0925').innerHTML")}"
+    # puts "INSIDE: #{page.evaluate_script("document.getElementById('invoice_cf0925_id').innerHTML")}"
+    # Page synchronization
+    page.evaluate_script("document.getElementById('invoice_cf0925_id').innerHTML")
     assert_select 'Request to Pay', selected: 'Joe 2016 2016-07-01 to 2017-06-14'
     click_link_or_button 'Save'
     assert_content 'Invoice saved.'
@@ -49,6 +51,7 @@ class InvoicesTest < PoltergeistTest
 
     # puts "INSIDE AGAIN: #{page.evaluate_script("document.getElementById('invoice_cf0925_id').innerHTML")}"
     # Force page synchronization
+    page.evaluate_script("document.getElementById('invoice_cf0925_id').innerHTML")
     page.evaluate_script("document.getElementById('invoice_cf0925_id').innerHTML")
     select 'Joe 2016 2016-07-01 to 2016-08-30', from: 'Request to Pay'
     click_link_or_button 'Save'
