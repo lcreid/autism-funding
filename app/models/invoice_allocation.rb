63ee0bffab4ad8c1f4efbebf9e965436dba08ced
Larry Reid <lcreid@jadesystems.ca>
2017-02-17 17:16:03 -0800

    Remove autosave: true from belongs_to.

--- app/models/invoice_allocation.rb
+++ app/models/invoice_allocation.rb
@@ -1,9 +1,9 @@
 ##
 # Join model for RTP and invoice
 class InvoiceAllocation < ApplicationRecord
-  belongs_to :cf0925, autosave: true, inverse_of: :invoice_allocations
+  belongs_to :cf0925, inverse_of: :invoice_allocations
   # delegate *Cf0925.column_names, to: :cf0925, prefix: true
-  belongs_to :invoice, autosave: true, inverse_of: :invoice_allocations
+  belongs_to :invoice, inverse_of: :invoice_allocations
 
   validates :cf0925_type,
             presence: true,
