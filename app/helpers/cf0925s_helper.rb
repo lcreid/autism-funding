4539c7bd79195117b4c4f5b8a73df68871940282
Larry Reid <lcreid@jadesystems.ca>
2017-02-23 13:37:04 -0800

    All buttons btn-primary now.

--- app/helpers/cf0925s_helper.rb
+++ app/helpers/cf0925s_helper.rb
@@ -18,10 +18,10 @@ module Cf0925sHelper
   # The buttons at the bottom of the form.
   def buttons(f, cf0925)
     # content_tag :div, class: 'form-inline' do
-    f.submit('Save', class: 'btn btn-default') +
-      f.button('Reset', type: 'reset', class: 'btn btn-default') +
-      print_button(cf0925) +
-      home_button
+    [f.submit('Save', class: 'btn btn-primary'),
+     f.button('Reset', type: 'reset', class: 'btn btn-primary'),
+     print_button(cf0925),
+     home_button].join(' ').html_safe
     # end
   end
 
@@ -229,7 +229,7 @@ module Cf0925sHelper
   end
 
   def cf0925_button(opts)
-    classes = 'btn btn-default'
+    classes = 'btn btn-primary'
     classes += ' ' + opts[:class] if opts[:class]
     yield classes
   end
